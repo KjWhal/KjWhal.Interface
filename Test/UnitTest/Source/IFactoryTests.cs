ffa20a319288d1bf16319f7bee05e1273080a975
Kevin Whalley <kjwhal@gmail.com>
2013-01-02 22:01:21 -0800

    Unit Tests
    Added unit tests to verify publicly exposed functionality

---
+++ Test/UnitTest/Source/IFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    using KjWhal.Interface;
+
+    /// <summary>
+    /// Container class for test methods of IFactory
+    /// </summary>
+    [TestClass]
+    public class IFactoryTests
+    {
+        /// <summary>
+        /// Verifies that Factory implements IFactory(T)
+        /// </summary>
+        [TestMethod]
+        public void IFactoryPubliclyAvailable()
+        {
+            IFactory<string> sut = new Factory();
+        }
+
+        /// <summary>
+        /// Verifies that IFactory(T) implements Initialize
+        /// </summary>
+        [TestMethod]
+        public void IFactoryRequiresInitializeImplementation()
+        {
+            IFactory<string> sut = new Factory();
+
+            var result = sut.Initialize();
+
+            Assert.AreEqual<string>(string.Empty, result);
+        }
+    }
+
+    private class Factory : IFactory<string>
+    {
+        internal string Initialize()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Test/UnitTest/Source/IRuntimeFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    using KjWhal.Interface;
+
+    /// <summary>
+    /// Container class for test methods of IFactory
+    /// </summary>
+    [TestClass]
+    public class IRuntimeFactoryTests
+    {
+        /// <summary>
+        /// Verifies that Factory implements IRuntimeFactory(TResult, TArgument)
+        /// </summary>
+        [TestMethod]
+        public void IRuntimeFactoryPubliclyAvailable()
+        {
+            IRuntimeFactory<string, string> sut = new Factory();
+        }
+
+        /// <summary>
+        /// Verifies that IRuntimeFactory(TResult, TArgument) implements Initialize
+        /// </summary>
+        [TestMethod]
+        public void IRuntimeFactoryRequiresInitializeImplementation()
+        {
+            IRuntimeFactory<string, string> sut = new Factory();
+
+            var result = sut.Initialize(string.Empty);
+
+            Assert.AreEqual<string>(string.Empty, result);
+        }
+    }
+
+    private class Factory : IRuntimeFactory<string, string>
+    {
+        internal string Initialize(string argument)
+        {
+            return string.Empty;
+        }
+    }
+}
