c307bfc84ed5a957a26876467105a81505dba99a
Kevin Whalley <kjwhal@gmail.com>
2013-01-02 21:47:38 -0800

    First commit
    Factory interfaces. A bit over-simplified but helpful for codifying the
    root of what a simple factory class does

---
+++ Source/IFactory.cs
@@ -0,0 +1,16 @@
+﻿
+namespace KjWhal.Interface
+{
+    /// <summary>
+    /// Contract for an instance which initializes new instances of types
+    /// </summary>
+    /// <typeparam name="TResult">Type to instantiate</typeparam>
+    public interface IFactory<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of TResult with run-time arguments from TArgument
+        /// </summary>
+        /// <returns>Instance of TResult</returns>
+        T Initialize();
+    }
+}
---
+++ Source/IRuntimeFactory.cs
@@ -0,0 +1,18 @@
+﻿
+namespace KjWhal.Interface
+{
+    /// <summary>
+    /// Contract for an instance which initializes new instances of types
+    /// </summary>
+    /// <typeparam name="TResult">Type to instantiate</typeparam>
+    /// <typeparam name="TArgument">Type representing run-time arguments used in initialization</typeparam>
+    public interface IRuntimeFactory<TResult, TArgument>
+    {
+        /// <summary>
+        /// Initializes a new instance of TResult with run-time arguments from TArgument
+        /// </summary>
+        /// <param name="argument">Run-time arguments for initializing a new instance ofTResult</param>
+        /// <returns>Instance of TResult</returns>
+        TResult Initialize(TArgument argument);
+    }
+}
